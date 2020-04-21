7a3faa6db9f0cc56a0acacca774443379d4ea39d
Whis <maxthanh123@gmail.com>
2020-04-21 19:34:20 +0700

    Add shit for pig

--- pig.js
+++ pig.js
@@ -1,9 +1,12 @@
 function Pig(name) {
     this.name = name;
+<<<<<<< HEAD
     this.die = false;
     this.stomach = [];
     Pig.prototype.shit = function(cat) {
         this.stomach.push(cat);
     }
+=======
+>>>>>>> parent of 410b638... Add pig module
 }
 file.export = pig;
